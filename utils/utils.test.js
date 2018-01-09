d389797db2614cb118f8f83fce50eb0f073b5e7a
Baz Jay <jaybaz1@gmail.com>
2018-01-09 22:02:37 +0000

    Added expect assertion library and moved test watch into scripts

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,39 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if(res !== 44){
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a numbers', () => {
     var res = utils.square(3);
 
-    if(res !== 9){
-        throw new Error(`Expected 9, but got ${res}.`);
-    }
+    expect(res).toBe(9).toBeA('number');
+});
+
+// it('should expect some values', () => {
+
+//     //expect(12).toNotBe(13);
+//     //expect({name: 'Baz'}).toEqual({name: 'Baz'});
+//     //expect({name: 'Baz'}).toNotEqual({name: 'Baz1'});
+//     //expect([2,3,4]).toInclude(3);
+//     //expect([2,3,4]).toExclude(5);
+//     expect({
+//         name: 'Baz',
+//         age: 25,
+//         location: 'London'
+//     }).toInclude({
+//         age: 25
+//     });
+// });
+
+it('should verify first and last names are set', () => {
+    let res = utils.setName({
+        age: 25,
+        location: 'London'
+    }, 'Baz Jay');
+
+    expect(res).toInclude({firstName: 'Baz', lastName: 'Jay'}).toBeA('object');
 });
